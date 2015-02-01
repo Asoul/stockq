b958ebedc4bd790aa6ee3190ad85fde5a3b649ae
asoul <b00902036@ntu.edu.tw>
2015-01-31 21:24:37 -0500

    unset git auto push

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 while :
 do
     scrapy crawl stockq
-    month=`date +%m`
-    day=`date +%d`
-    git add .
-    git commit -m "update to $month/$day"
-    git push
+    #month=`date +%m`
+    #day=`date +%d`
+    #git add .
+    #git commit -m "update to $month/$day"
+    #git push
     # 24 x 60 x 60 = 86400
     sleep 80000
 done

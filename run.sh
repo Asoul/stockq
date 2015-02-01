4a2a818488633355c516941c4c686e3efd2266cd
Asoul <b00902036@ntu.edu.tw>
2015-02-01 10:10:04 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 while :
 do
     scrapy crawl stockq
+    month=`date +%m`
+    day=`date +%d`
+    git add .
+    git commit -m "update to $month/$day"
+    git push
     # 24 x 60 x 60 = 86400
     sleep 86300
 done
